bf5b29d1e7725eacf091fa7f5c4bb21212ee5d6e
Manuel Mato <mnl.mato@gmail.com>
2022-07-08 12:53:39 +0200

    Remove unnecessary import

--- app/src/main/java/com/harrypotter/features/characters/ui/CharactersActivity.kt
+++ app/src/main/java/com/harrypotter/features/characters/ui/CharactersActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.ViewGroup
 import android.widget.ProgressBar
 import androidx.activity.viewModels
-import androidx.annotation.VisibleForTesting
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.DividerItemDecoration
