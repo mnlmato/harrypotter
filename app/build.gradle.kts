9be3d70de446bcd5d6e9c601cd7226c91faa0ece
Manuel Mato <manuel.mato@free-now.com>
2024-04-28 11:47:13 +0200

    Add paparazzi dependency and test the error state into CharactersActivity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id(libs.plugins.dagger.hilt.get().pluginId)
     id(libs.plugins.kotlin.android.get().pluginId)
     id(libs.plugins.kotlin.kapt.get().pluginId)
+    id(libs.plugins.paparazzi.get().pluginId)
 }
 
 android {
@@ -55,6 +56,26 @@ android {
         unitTests {
             isIncludeAndroidResources = true
             isReturnDefaultValues = true
+        }
+    }
+
+    // Paparazzi: Remove when https://github.com/google/guava/issues/6567 is fixed.
+    // See also: https://github.com/google/guava/issues/6801.
+    dependencies.constraints {
+        testImplementation("com.google.guava:guava") {
+            attributes {
+                attribute(
+                    TargetJvmEnvironment.TARGET_JVM_ENVIRONMENT_ATTRIBUTE,
+                    objects.named(
+                        TargetJvmEnvironment::class.java,
+                        TargetJvmEnvironment.STANDARD_JVM
+                    )
+                )
+            }
+            because(
+                "Paparazzi's layoutlib and sdk-common depend on Guava's -jre published variant." +
+                        "See https://github.com/cashapp/paparazzi/issues/906."
+            )
         }
     }
 
---
+++ app/src/test/java/com/harrypotter/features/characters/main/ui/CharactersErrorScreenTest.kt
@@ -0,0 +1,34 @@
+package com.harrypotter.features.characters.main.ui
+
+import app.cash.paparazzi.DeviceConfig
+import app.cash.paparazzi.Paparazzi
+import com.android.ide.common.rendering.api.SessionParams
+import com.harrypotter.designsystem.theme.CustomTheme
+import com.harrypotter.features.characters.main.ui.design.CharactersScreen
+import com.harrypotter.features.characters.main.vm.model.CharactersState
+import org.junit.Rule
+import org.junit.Test
+
+class CharactersErrorScreenTest {
+
+    @get:Rule
+    val paparazzi = Paparazzi(
+        deviceConfig = DeviceConfig.PIXEL_5,
+        theme = "CustomTheme",
+        renderingMode = SessionParams.RenderingMode.SHRINK,
+        showSystemUi = false,
+    )
+
+    @Test
+    fun `test characters list when state is error`() {
+        paparazzi.snapshot {
+            CustomTheme {
+                CharactersScreen(
+                    charactersState = CharactersState.Error,
+                    {},
+                    {},
+                )
+            }
+        }
+    }
+}
